7c70f75cdca8d2859a9bb8f750ebd27d77d43629
test <test@test.com>
2023-01-20 14:57:20 +0800

    init

---
+++ 01.Javascript第一个code.js
@@ -0,0 +1,2 @@
+x=1,y=5
+console.log(x+y)
---
+++ 03.一个外部JS.js
@@ -0,0 +1,9 @@
+function myExternalFunction(x)
+{
+    document.getElementById(x).innerHTML="我的第一个Javascript_External"
+}
+
+function 测试对象引用方法()
+{
+    window.alert("play games");
+}
